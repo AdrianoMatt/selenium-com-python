981e93a8ebfbf71410b028c03bcdabb7b07fea24
AdrianoMatt <snoot.adriano@gmail.com>
2024-06-02 19:22:35 -0300

    commit 3

--- test_comandos_navegacao.py
+++ test_comandos_navegacao.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 #browser = webdriver.Chrome() caso fosse definir browser
 
@@ -11,5 +13,6 @@ driver = webdriver.Chrome(service=service, options=options)
 # get() acessa o parametro inserido
 driver.get("https://www.cartoesmaisbarato.com.br/")
 time.sleep(4)
-driver.find_element(By.XPATH, "/html/body/div[2]/header/section[2]/div[4]/div/div[3]/div[1]/span[2]/a[1]").click()
+#driver.find_element(By.XPATH, "/html/body/div[2]/header/section[2]/div[4]/div/div[3]/div[1]/span[2]/a[1]").click()
+driver.find_element(By.XPATH, "//a[@class='js-trigger-login']").click()
 time.sleep(2)
