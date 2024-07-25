c03424498d3be583422ce080a71840e95b8ada63
AdrianoMatt <snoot.adriano@gmail.com>
2024-07-24 21:30:34 -0300

    commit 24/07

--- test_comandos_navegacao.py
+++ test_comandos_navegacao.py
@@ -15,4 +15,5 @@ driver.get("https://www.cartoesmaisbarato.com.br/")
 time.sleep(4)
 #driver.find_element(By.XPATH, "/html/body/div[2]/header/section[2]/div[4]/div/div[3]/div[1]/span[2]/a[1]").click()
 driver.find_element(By.XPATH, "//a[@class='js-trigger-login']").click()
+
 time.sleep(2)
